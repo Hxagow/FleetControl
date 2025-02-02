1108edc26548c891babb5b213732826babc3c6b9
Hxagow <bublicaflorentpro@gmail.com>
2025-02-02 23:49:24 +0100

    Switch back to PostgreSQL database configuration

--- src/fleet_control/settings.py
+++ src/fleet_control/settings.py
@@ -100,9 +100,21 @@ from decouple import config
 print(config('DB_NAME'))
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': config('DB_NAME'),
+
+        'USER': config('DB_USER'),
+
+        'PASSWORD': config('DB_PASSWORD'),
+
+        'HOST': config('DB_HOST', default='localhost'),
+
+        'PORT': config('DB_PORT', default='5432'),
+
     }
 
 }
